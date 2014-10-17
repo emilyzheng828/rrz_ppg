be741e45b595c7664f52d71843897c761381c3c9
Emily Zheng <emily.zheng828@gmail.com>
2014-10-17 20:26:05 +0100

    first file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RrzPpg::Application.config.secret_key_base = '0e046df503d89ddb9b7886ba54d5526ffd1d04ec09556e103ff67cf63eee5d5d195491bcf7ecf87bd46f66ffa57d7395e7c51ffd21c51c7c32c268f78a207f9c'
